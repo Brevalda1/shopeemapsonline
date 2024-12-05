e8c51d014e504e507293e40c617bb3e772c1044c
Brevalda1 <114040269+Brevalda1@users.noreply.github.com>
2024-12-05 15:10:35 +0700

    midtrans test

--- shopeemapsonline/config/midtrans.php
+++ shopeemapsonline/config/midtrans.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'server_key' => env('MIDTRANS_SERVER_KEY', 'SB-Mid-server-7jqDqMUWF5PpWhb01kYpyg_j'),
-    'client_key' => env('MIDTRANS_CLIENT_KEY', 'SB-Mid-client-6AUGjhjpRblPK_j6'),
-    'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
+    'server_key' => env('MIDTRANS_SERVER_KEY', 'Mid-server-qh4rBGaMRIhTvDXvJnizVhWR'),
+    'client_key' => env('MIDTRANS_CLIENT_KEY', 'Mid-client-mC9P1VXONEzAQyJE'),
+    'is_production' => env('MIDTRANS_IS_PRODUCTION', true),
     'is_sanitized' => true,
     'is_3ds' => true,
-]; 
+];
